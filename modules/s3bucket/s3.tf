30978634528f68386ee42897b02b94fbc6bde0bc
Sameem Mohamed Rafeeq <sameem.aws21@gmail.com>
2025-08-19 07:57:09 -0400

    adding s3 bucket and associating it to ec2 instance

---
+++ modules/s3bucket/s3.tf
@@ -0,0 +1,28 @@
+#creating IAM role for EC2 instance
+resource "aws_iam_role" "sam_ec2_role" {
+  name = "sam_ec2_role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+}
+
+#attaching policy to IAM role
+resource "aws_iam_role_policy_attachment" "sam_ec2_policy_attachment" {
+  role       = aws_iam_role.sam_ec2_role.name #role we created above 
+  policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
+}
+
+#creating IAM instance profile
+resource "aws_iam_instance_profile" "sam_ec2_instance_profile" {
+  name = "sam_ec2_instance_profile"
+  role = aws_iam_role.sam_ec2_role.name
+}
